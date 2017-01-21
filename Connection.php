5921adee997243ec4b95267400e32d80021b43f5
Gracia Estelle <gracialatumahina@live.nl>
2017-01-21 05:17:14 +0100

    Create Connection.php

---
+++ Connection.php
@@ -0,0 +1,16 @@
+class Connection
+{
+
+    public static function Make($config){
+        try{
+            return new PDO(
+                $config['connection']. ';dbname='.
+                $config['dbname'],
+                $config['user'],
+                $config['password'],
+                $config['options']);
+        }catch (PDOException $e){
+            die($e->getMessage());
+        }
+    }
+}
